96947e15b4c0d7c1a5dfa5050e9a1540e379529c
Hameedat Oyewopo <hameedattimileyinoyewopo@gmail.com>
2025-04-28 12:30:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.get('/', async(req, res) => {
+    try{
+        const result = await axios.get(' https://api.openuv.io/api/v1/uv?lat=:lat&lng=:lng&alt=:alt&dt=:dt', {
+            headers: {
+                'x-access-token': 'openuv-1ehf2xrma0y83s5-io',
+                'Content-Type': 'application/json'
+            }
+    });
+        res.render('index.ejs', {data: result.data});
+    }catch (error){
+        console.error('Error fetching data from OpenUV API:', error);
+        res.status(500).send('index.ejs', {error: 'Error fetching data from OpenUV API'});
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
