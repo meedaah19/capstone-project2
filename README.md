3760aa9067303164e70ca9a57fba40c0f24d7d99
Hameedat Oyewopo <hameedattimileyinoyewopo@gmail.com>
2025-05-03 13:31:23 +0100

    refactor README and enhance HTML templates with comments for better clarity

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# backend
+UV INDEX CHECKER
+
+
+To get started:
+run npm install to install all available dependecies
+then run node index.js to start the sever
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// import files
 import express from 'express';
 import axios from 'axios';
 import bodyParser from 'body-parser';
@@ -5,18 +6,18 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public')); // to have access to the files inside public folder
+app.use(bodyParser.urlencoded({ extended: true })); // middleware for processing request
 
 app.get('/', (req, res) => {
-  res.render('index.ejs'); 
+  res.render('index.ejs'); //server rendering the home page
 });
 
-app.post('/uv', async(req, res) => {
+app.post('/uv', async(req, res) => { //server rendering the uv page
     const { lat, lng } = req.body;
 
-    try{
-        const result = await axios.get(`https://api.openuv.io/api/v1/uv?lat=${lat}&lng=${lng}`, {
+    try{ //put in a try an catch error to catch error if the request fail
+        const result = await axios.get(`https://api.openuv.io/api/v1/uv?lat=${lat}&lng=${lng}`, { // get request use to get the api used
             headers: {
                 'x-access-token': 'openuv-1ehf2xrma0y83s5-io',
                 'Content-Type': 'application/json'
@@ -24,13 +25,13 @@ app.post('/uv', async(req, res) => {
     });
         const data = result.data.result;
 
-        const sunriseUTC = new Date(data.sun_info.sun_times.sunrise);
-        const sunsetUTC = new Date(data.sun_info.sun_times.sunset);
-        const maxUV = new Date(data.uv_max_time);
-        const uvIndex = data.uv.toFixed(0);
-        const uvMaxIndex = data.uv_max.toFixed(0);
+        const sunriseUTC = new Date(data.sun_info.sun_times.sunrise); //conversion of the sunrise time
+        const sunsetUTC = new Date(data.sun_info.sun_times.sunset); // conversion of the sunset time
+        const maxUV = new Date(data.uv_max_time); // conversion od the max uv time
+        const uvIndex = data.uv.toFixed(0); // convert the uv index to 0 standard form
+        const uvMaxIndex = data.uv_max.toFixed(0); // convert the uv max index to 0 standard time
 
-        const options = {
+        const options = { 
             hour: 'numeric',
             minute: 'numeric',
             hour12: true,
@@ -54,7 +55,7 @@ app.post('/uv', async(req, res) => {
           
           const uvLevel = getUvLevel(data.uv);
         
-        res.render('uv.ejs', {
+        res.render('uv.ejs', { // pass all the data needed in the uv ejs
             data,
             uvIndex,
             uvMaxIndex,
@@ -63,11 +64,11 @@ app.post('/uv', async(req, res) => {
             sunset: formattedSunset,
             maxUV: formattedMaxUV,
         });
-    }catch (error){
+    }catch (error){ // pass the error data to uv ejs to display error message
         res.status(500).render('uv.ejs', {error: 'Error fetching data from OpenUV API'});
     }
 })
 
-app.listen(port, () => {
+app.listen(port, () => { // the server is running on port 3000
     console.log(`Server is running on port ${port}`);
 })
